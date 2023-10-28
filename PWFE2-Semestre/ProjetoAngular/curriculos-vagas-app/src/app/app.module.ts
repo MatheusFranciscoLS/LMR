3128eb94a5d522f2b5379fe1926ea9fb0607de03
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2023-10-27 21:48:32 -0300

    27/10/23

---
+++ PWFE2-Semestre/ProjetoAngular/curriculos-vagas-app/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { VagasComponent } from './vagas/vagas.component';
+import { CurriculosComponent } from './curriculos/curriculos.component';
+import { FooterComponent } from './basic/footer/footer.component';
+import { HeaderComponent } from './basic/header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    VagasComponent,
+    CurriculosComponent,
+    FooterComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
