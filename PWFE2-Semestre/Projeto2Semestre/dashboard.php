f8148502badf11340986d304b21434e367759de9
MatheusFranciscoLS <matheusfran.ls@gmail.com>
2023-12-01 21:53:28 -0300

    01/12/23

--- PWFE2-Semestre/Projeto2Semestre/dashboard.php
+++ PWFE2-Semestre/Projeto2Semestre/dashboard.php
@@ -13,7 +13,7 @@
       // Função para redirecionar para a página index após 3 segundos
       function redirecionarParaIndex() {
         setTimeout(function() {
-          window.location.href = "http://localhost/MegaPetMartz/index.html";
+          window.location.href = "http://localhost/Projeto2Semestre/index.html";
         }, 3000); // 3000 milissegundos = 3 segundos
       }
 
